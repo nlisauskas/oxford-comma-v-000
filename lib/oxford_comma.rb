894fe2d2130bc2fb7211c39a3fe7e0dea60dd2c5
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 13:51:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+array = [1,2,3]
 
+def oxford_comma(array)
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    final_element = array.pop()
+    new_array = array.join(", ")
+    new_array << ", and #{final_element}"
+    return new_array
+  end
 end
